49cd7a3134a50184ec21158a78169c2b6fa28455
Alexander Richey <alrichey@amazon.com>
2020-05-18 22:23:37 -0700

    return html page

--- hello.py
+++ hello.py
@@ -1,14 +1,28 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
 COUNT = 0
 
+
 @app.route("/")
 def hello():
     global COUNT
     COUNT += 1
-
-    app.logger.debug("Count: %s", COUNT)
-
-    return "Hello world!"
+    danger = os.environ
+    return f"""
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>hello world</title>
+    <!-- <link rel="stylesheet" href="https://newcss.net/new.min.css"> -->
+  </head>
+  <body>
+    <h1>Hello world {COUNT} times!</h1>
+    <p>Here is my environment:</p>
+    <p><pre>{danger}</pre></p>
+  </body>
+</html>
+    """
